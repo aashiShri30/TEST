91508bc5f9d79ea2f0b15854ed8fea5da6e4c541
aashiShri30 <68980433+aashiShri30@users.noreply.github.com>
2020-07-30 15:45:08 +0530

    Create print_freq.c

---
+++ print_freq.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include<ctype.h>
+#include<stdlib.h>
+
+void print_freq(char* line)
+{
+char i;int j,count=0;
+
+char ch;
+printf("hello");
+for(i='a';i<'z';i++)
+{count=0;
+for(j=0;*(line+j)!='\0';j++)
+{
+ ch=tolower(*(line+j));
+if(ch==i)
+count++;
+}
+if(count>0)
+printf("count of %c = %d \n ",i,count);
+}
+return 0;
+}
+int main(void) {
+	// your code goes here
+	return 0;
+	char *cha=(char *)malloc(sizeof(char)*100);
+	scanf("%s",cha);
+	print_freq(cha);
+	return 0;
+}
