081a7afc61864250c0583722812916a1ed4a5ca0
aashiShri30 <68980433+aashiShri30@users.noreply.github.com>
2020-07-30 18:51:10 +0530

    Create parity.c

---
+++ parity.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<string.h>
+int main(void) {
+int n,k,t=1,p=0,i,d;
+printf("enter the no \n");
+scanf("%d",&n);
+k=n^(0x00);
+for(i=0;i<7;i++)
+{
+    d=t<<i;
+    if((k&d)==d)
+    p++;
+}
+if(p%2!=0)
+n=n+0x80;
+
+printf("%x\n",n);
+return 0;
+}
+
