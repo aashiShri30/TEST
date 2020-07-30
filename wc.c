989b8bda74ab4df821a38db4089a07f03e558cfd
aashiShri30 <68980433+aashiShri30@users.noreply.github.com>
2020-07-30 16:27:07 +0530

    Create wc.c

---
+++ wc.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<stdlib.h>
+int word_count(char *line)
+ {
+int i,c=0; 
+for(i=0;*(line+i)!='\0';i++)
+{
+if(*(line+i)==' '||*(line+i)=='\n'||*(line+i)=='\t')
+c++;
+}
+printf("%d \n",(c+1));
+return 0;
+}
+int main(void)
+{
+    char *line;
+    line=(char *)malloc(sizeof(char)*200);
+    scanf("[^\n%c]",line);
+    word_count(line);
+}
