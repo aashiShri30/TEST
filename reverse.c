acb7ede766f2e15aba649d32e1677e535f6cbcf7
aashiShri30 <68980433+aashiShri30@users.noreply.github.com>
2020-07-30 09:18:16 +0530

    Create reverse.c

---
+++ reverse.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<string.h>
+
+int main(void) {
+	// your code goes here
+	
+	char c[20]="Aashiy";
+	char *t;
+	int n= strlen(c);
+	int i;
+//	printf("%d",n);
+	for(i=0;i<n/2;i++)
+	{
+	    *t=*(c+i);
+	    *(c+i)=*(c+n-i-1);
+	    *(c+n-1-i)=*t;
+	    
+	}
+	printf("%s",c);
+	return 0;
+}
